3ae63e56117af0d0567beda8f94de4a35e0ea182
Mónika Lombos <lombos.monika@gmail.com>
2016-07-19 13:32:42 +0200

    create Models: Applicant, Mentor, School, City OK

--- models.py
+++ models.py
@@ -3,10 +3,50 @@ from peewee import *
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('dbname', user='dbuser')
+db = PostgresqlDatabase('lombocska', user='lombocska')
 
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
     class Meta:
         database = db
+
+
+class City(BaseModel):
+
+    name = CharField()
+
+
+class School(BaseModel):
+
+    school_id = PrimaryKeyField()
+    name = CharField()
+    city = ForeignKeyField(City)
+
+
+class Applicant(BaseModel):
+
+    applicant_id = PrimaryKeyField()
+    first_name = CharField()
+    last_name = CharField()
+    year_of_birth = DateField()
+    gender = CharField()
+    adress = CharField()
+    is_valued = BooleanField(default=False)
+    accepted = BooleanField(default=False)
+
+
+class Mentor(BaseModel):
+    mentor_id = PrimaryKeyField()
+    first_name = CharField()
+    last_name = CharField()
+    school_id = ForeignKeyField(School)
+
+
+
+
+
+
+
+
+# class InterviewSlot(BaseModel):
