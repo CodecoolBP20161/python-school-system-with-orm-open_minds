32ab17aee0ecd51471a27e3be6386f60b3948791
Dávid Fehér <feherkeee@gmail.com>
2016-07-20 16:15:29 +0200

    status can be updated

--- application_code_gen.py
+++ application_code_gen.py
@@ -22,10 +22,11 @@ def app_code_generator():
 
 
 def assign_app_codes():
-    for applicant in Applicant.select():
-        if applicant.status == 'new':
+    for applicant in Applicant.select().where(Applicant.status == 'new'):
             applicant.application_code = app_code_generator()
+            applicant.status = 'in progress'
             applicant.save()
+
 
 
 assign_app_codes()
