35ccdb85161dd149b3a8a69f60c6fc4e391c6bd0
ppayter-cc <pomsarpeter@gmail.com>
2016-07-20 15:09:05 +0200

    assign_app_code function implemented

--- application_code_gen.py
+++ application_code_gen.py
@@ -4,21 +4,27 @@ import uuid
 from models import *
 
 
-new_applicants = {}
 existing_application_codes = []
-
-
-def find_new_applicants():
-    for applicant in Applicant.select():
-        new_applicants[applicant.id] = applicant.application_code
 
 
 def get_ids():
     for applicant in Applicant:
         existing_application_codes.append(applicant.application_code)
+    return
 
 
 def app_code_generator():
-    id_temp = str(uuid.uuid4())[:6]
-    if id_temp not in existing_ids:
-        return id_temp
+    new_code = str(uuid.uuid4())[:6]
+    if new_code not in existing_application_codes:
+        return new_code
+    else:
+        return app_code_generator()
+
+
+def assign_app_codes():
+    for applicant in Applicant.select():
+        get_ids()
+        applicant.update(application_code=app_code_generator()).where(applicant.status == 'new').execute()
+        applicant.update(status='in progress').execute()
+
+assign_app_codes()
