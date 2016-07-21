5aa3e7f4b42649d76e407ff235baabdec8f8099f
Matolay <matolaypal@gmail.com>
2016-07-21 10:28:21 +0200

    Base main for user

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from models import *
+from contact_school import get_nearest_school
+from application_code_gen import assign_app_codes
+from example_data import *
 
-# Write here your console application
+
+add_city()
+print("Cities created.")
+input("[Press Enter]")
+add_schools()
+print("Schools created.")
+input("[Press Enter]")
+add_mentors()
+print("Mentors created.")
+input("[Press Enter]")
+add_applicants()
+print("Applicants created.")
+input("[Press Enter]")
+assign_app_codes()
+print("Applicants get code.")
+input("[Press Enter]")
+get_nearest_school()
