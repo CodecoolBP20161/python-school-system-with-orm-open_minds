fb9e151b975640f32fdb1ba37c90b3414a2e74a2
Mónika Lombos <lombos.monika@gmail.com>
2016-07-21 16:52:04 +0200

    build function implement

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
-# This script can create the database tables based on your models
-
 from models import *
 
-db.connect()
-# List the tables here what you want to create...
-db.drop_tables([Applicant, Mentor, School, City, InterviewSlot], safe=True)
-db.create_tables([Applicant, Mentor, School, City, InterviewSlot], safe=True)
+
+def build():
+
+    db.connect()
+    # List the tables here what you want to create...
+    db.drop_tables([Applicant, Mentor, School, City, InterviewSlot], safe=True)
+    db.create_tables([Applicant, Mentor, School, City, InterviewSlot], safe=True)
