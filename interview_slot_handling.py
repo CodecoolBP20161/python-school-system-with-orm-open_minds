57993dbfd3632ae625746dad2ff849cd13927f0c
Mónika Lombos <lombos.monika@gmail.com>
2016-07-21 15:58:07 +0200

    implement function check_reserved_interviews AND organized into class

--- interview_slot_handling.py
+++ interview_slot_handling.py
@@ -1,33 +1,35 @@
 # Write a new script which detects unreserved interview slots and reserves an interview slot for the applicant
 from models import *
 
-applicants = {}
 
+class InterviewHandling:
 
-def check_reserved_interviews():
-    for applicant in Applicant.select():
-        applicants[applicant.application_code] = applicant.assigned_school
-    for interview in InterviewSlot.select().where(InterviewSlot.applicant != None):
-        if interview.applicant in applicants:
-            del applicants[interview.applicant]
+    applicants = {}
 
+    @staticmethod
+    def check_reserved_interviews():
+        for applicant in Applicant.select():
+            InterviewHandling.applicants[applicant.application_code] = applicant.assigned_school
+        for interview in InterviewSlot.select().where(InterviewSlot.applicant != None):
+            if interview.applicant in InterviewHandling.applicants:
+                del InterviewHandling.applicants[interview.applicant]
 
-def interview_slot_handling():
-    for element in applicants:
-        for interview in InterviewSlot.select().where(InterviewSlot.applicant == None):
-            if interview.school_name == applicants[element]:
-                interview.applicant = element
-                interview.save()
-                break
+    # @staticmethod
+    # def interview_slot_handling():
+        for element in InterviewHandling.applicants:
+            for interview in InterviewSlot.select().where(InterviewSlot.applicant == None):
+                if interview.school_name == InterviewHandling.applicants[element]:
+                    interview.applicant = element
+                    interview.save()
+                    break
 
+    # @staticmethod
+    # def find_applicants_without_slot():
+        for interview in InterviewSlot.select().where(InterviewSlot.applicant != None):
+            try:
+                del InterviewHandling.applicants[interview.applicant]
+            except KeyError:
+                pass
 
-def find_applicants_without_slot():
-    for interview in InterviewSlot.select().where(InterviewSlot.applicant != None):
-        print(interview.applicant)
-        try:
-            del applicants[interview.applicant]
-        except KeyError:
-            pass
-
-    for element in applicants:
-        print("Unfortunately we haven't free interview slot currently for {}".format(element))
+        for element in InterviewHandling.applicants:
+            print("Unfortunately we haven't free interview slot currently for {}".format(element))
