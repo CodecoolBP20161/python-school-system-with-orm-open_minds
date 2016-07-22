4ac2ba80315e4011ad332941595339d789e569c0
Matolay <matolaypal@gmail.com>
2016-07-22 11:40:07 +0200

    Pep8 clean and added comments

--- interview_slot_handling.py
+++ interview_slot_handling.py
@@ -1,4 +1,3 @@
-# Write a new script which detects unreserved interview slots and reserves an interview slot for the applicant
 from models import *
 
 
@@ -7,15 +6,18 @@ class InterviewHandling:
     applicants = {}
 
     @staticmethod
-    def check_reserved_interviews():
+    def interview_handling():
+        # This collect the all applicants and organize into "applicants" dict.
+        # Example: applicants[key] = application_code, applicants[value] = assigned_school
         for applicant in Applicant.select():
             InterviewHandling.applicants[applicant.application_code] = applicant.assigned_school
+        # Who already have reserved interview slot delete them form the dictionary.
         for interview in InterviewSlot.select().where(InterviewSlot.applicant != None):
             if interview.applicant in InterviewHandling.applicants:
                 del InterviewHandling.applicants[interview.applicant]
 
-    # @staticmethod
-    # def interview_slot_handling():
+        # Who haven't reserved interview slot get a new appointment.
+        # After that break the subloop and looking fo an other one.
         for element in InterviewHandling.applicants:
             for interview in InterviewSlot.select().where(InterviewSlot.applicant == None):
                 if interview.school_name == InterviewHandling.applicants[element]:
@@ -23,13 +25,14 @@ class InterviewHandling:
                     interview.save()
                     break
 
-    # @staticmethod
-    # def find_applicants_without_slot():
+        # This checks the applicants who didn't get interview slot.
+        # Then deletes the ones who has interview slot.
         for interview in InterviewSlot.select().where(InterviewSlot.applicant != None):
             try:
                 del InterviewHandling.applicants[interview.applicant]
             except KeyError:
                 pass
 
+        # And print the dictionary element.
         for element in InterviewHandling.applicants:
             print("Unfortunately, we have no free interview slot left currently for {}".format(element))
