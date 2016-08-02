94840bc5ee2ea35061781527ff9383417859f5aa
Matolay <matolaypal@gmail.com>
2016-08-02 15:08:52 +0200

    Implement City model: organized own datas and methods

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
 from models import *
 from school_model import School
+from city_model import City
 from mentor_model import Mentor
 from applicant_model import Applicant
 from interviewslot_model import InterviewSlot
 
 db = PostgresqlDatabase("")
 db.connect()
-db.drop_tables([School, Mentor, Applicant, InterviewSlot], safe=True)
-db.create_tables([School, Mentor, Applicant, InterviewSlot], safe=True)
+db.drop_tables([School, City, Mentor, Applicant, InterviewSlot], safe=True)
+db.create_tables([School, City, Mentor, Applicant, InterviewSlot], safe=True)
 
 School.add_schools()
 Mentor.add_mentors()
 InterviewSlot.add_interview_slot()
+City.add_cities()
