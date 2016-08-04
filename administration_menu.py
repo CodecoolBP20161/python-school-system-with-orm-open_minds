da3ed5ddcd3b7dd15d09b0efd38f40e76f27bd64
Matolay <matolaypal@gmail.com>
2016-08-04 13:38:51 +0200

    filter_by_mentor_name MODIFIED filter_by_mentor_id; Applicant print FIXED

--- administration_menu.py
+++ administration_menu.py
@@ -66,20 +66,14 @@ def filter_by_school():
     answer = input("Enter a school id: ")
     for applicant in Applicant.select().where(Applicant.assigned_school == answer):
         print(applicant)
-        # print(Applicant.assigned_school.name)
 
 
-def filter_by_mentor_name():
-    """Filter applicants by assigned mentors, IT ISN'T WORKING!"""
-    answer = input("Enter mentor's name: ")
-    # for applicant in Applicant.select().join(InterviewSlot,
-    #                                          on=(Applicant.application_code==InterviewSlot.applicant))\
-    #         .where(InterviewSlot.mentor.contains(answer)):
-    for applicant in Applicant.select().join(Mentor,
-                                             on=(Applicant.assigned_school==Mentor.school_id))\
-            .where(Applicant.assigned_school.contains(answer)):
-        print(applicant)
-
+def filter_by_mentor_id():
+    """Filter applicants by assigned mentors"""
+    answer = int(input("Enter mentor's id: "))
+    for mentor in Mentor.select().where(Mentor.id == answer):
+        for element in mentor.interviews:
+            print(element.applicants.get())
 
 # This library allows you to choose an operation.
 menu = OrderedDict([
@@ -88,5 +82,5 @@ menu = OrderedDict([
     ('3', filter_by_location),
     ('4', filter_by_name),
     ('5', filter_by_school),
-    ('6', filter_by_mentor_name),
+    ('6', filter_by_mentor_id),
 ])
