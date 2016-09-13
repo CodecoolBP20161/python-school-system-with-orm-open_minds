5b881b91d86d7b128c221a4b1296c348ad855289
Codecool-Palko <matolaypal@gmail.com>
2016-09-13 14:28:46 +0200

    signup_db_query changed applicant_signup and replaced here AND filter_db_query changed admin_filter and replaced here

--- controller_applicant.py
+++ controller_applicant.py
@@ -1,31 +1,4 @@
-from peewee import *
-
-
-def init_db():
-    try:
-        with open('db.nfo') as f:
-           db_name = f.read()
-    except FileNotFoundError:
-        with open('db.nfo', 'w') as f:
-            db_name = f.write(input("What's name your database?\n"))
-    return db_name
-init_db()
-psql_db = PostgresqlDatabase(init_db())
-psql_db.connect()
-
-
-def create_table():
-    from model_cityl import City
-    from model_school import School
-    from model_mentor import Mentor
-    from model_applicant import Applicant
-    from interviewslot_model import InterviewSlot
-
-    psql_db.drop_tables([School, City, Mentor, Applicant, InterviewSlot], safe=True)
-    psql_db.create_tables([School, City, Mentor, Applicant, InterviewSlot], safe=True)
-
-
-def signup_db_query(data):
+def applicant_signup(data):
     from model_applicant import Applicant
 
     data[0]['application_code'] = None
@@ -38,7 +11,7 @@ def signup_db_query(data):
     Applicant.assign_interview_slot()
 
 
-def filter_db_query(data):
+def admin_filter(data):
     from model_applicant import Applicant
     from model_mentor import Mentor
     result = []
